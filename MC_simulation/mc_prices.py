c50cdd6f5623582b0101e09708f0d258e96942a7
Daniel Lüke <daniel.lueke@t-online.de>
2019-12-04 16:53:27 +0100

    Add current work to repo.

---
+++ MC_simulation/mc_prices.py
@@ -0,0 +1,41 @@
+def draw_skill_prices(T=25, J=2, seed=555):
+    """ Draws initial skill prices and simulates random prices changes.
+    Agruments:
+        T:              Number of periods
+
+    Returns:
+        pi1, pi2:       Prices for tasks 1 and 2
+
+    Assumptions:
+        (1) Initial Skill Prices
+        (2) Chages in Skill Prices are normal distributed
+    """
+    # import packages
+    import numpy as np
+
+    # Set seed.
+    np.random.seed(seed)
+
+    # Define time horizon, assume we can observe 25 years
+    T = T
+
+    # Define number of Tasks
+    J = J
+
+    # Set initial task prices
+    # Assume task 1 (social) has a lower price than 2 (non-social)
+    pi1_0 = 1
+    pi2_0 = 2
+
+    # Draw price changes
+    d_pi = np.around(np.random.normal(size=(J, T-1)), 2)
+
+    # Define price array
+    pi = np.empty([J, T])
+    pi[:, 0] = pi1_0, pi2_0
+
+    # Calculate prices in each period
+    for t in range(1, T):
+        pi[:, t] = pi[:, t-1] + d_pi[:, t-1]
+
+    return pi
---
+++ MC_simulation/mc_skills.py
@@ -0,0 +1,57 @@
+def draw_skills(n, seed=555):
+    """ This script draws two correlated normal distributed skills and
+    calculates their percentiles.
+    Arguments:
+
+    Returns:
+
+    Assumptions:
+        (1) FOr now: 2 Tasks, "nonsocial" and "social"
+    """
+    # Import packages
+    import numpy as np
+
+    # Set parameter
+    np.random.seed(seed)
+    n = n  # no. of observations
+    #J = 2  # For now 2 different tasks
+
+    # --------------- Data generating process:
+    # --- Generate Skills (for now time-invariant)
+    # This section is based purely on my economic intuition.
+    # For now, assume that skills are positively correlated
+    skills_var = np.array([[1, 0.3], [0.3, 1]])
+
+    # I standardize skills to have mean 0
+    skills_mean = np.array([0, 0])
+
+    # Draw skills for n observations
+    skills = np.random.multivariate_normal(
+        mean=skills_mean,
+        cov=skills_var,
+        size=(40)
+        )
+
+    # Calculate skill percentiles
+    # Problem with percentiles: If the skill-percentile of a worker impacts her
+    # wage, skill accumulation can only impact wage, for a growth in skills
+    # in comparison to all other workers. Skill growth would in this case
+    # always be an improvement in comparison to all other workers.
+    def calc_percentile(array):
+        """ This function calculates the percentile of each value of the inputarray.
+        Input:
+            array - input array with values that will be mapped
+                    into percentiles.
+        Output:
+            percentiles -   array that contains the percentiles for each value
+                            of the
+            input array.
+        """
+        # Get the number of columns in the array
+        n, J = np.shape(array)
+        percentiles = np.empty([n, J])
+        for k in range(J):
+            for i in range(n):
+                percentiles[i, k] = sum(array[:, k] < array[i, k]) / n
+        return percentiles
+    return calc_percentile(skills)
