490e7eea247721cc20348f554b38097039972c54
Daniel Lüke <daniel.lueke@t-online.de>
2019-12-06 15:51:32 +0100

    Reworked to now work for any T periods.

--- MC_simulation/mc_optimal_wage_choice.py
+++ MC_simulation/mc_optimal_wage_choice.py
@@ -1,14 +1,22 @@
-def mc_optimal_wage_choice():
+def mc_optimal_wage_choice(n, T, J=2, seed=555):
     '''
     This script draws the simulated skills and skill prices around
     returns the optimally chosen worktime shares, as well as resulting wages
-    and utility.
+    and utility. This is done for all time periods.
+    Results are returned as multiindex-dataframe.
 
-    Input:
+    Arguments:
+        seed            needed for skill and price simulations
+        n               number of individuals to simulate
+        T               time periods to simulate
 
-    Output:
+    Returns:
+        Hierachical dataframe with individuals as first level index and
+        parameters on second level. One column per period.
 
     Assumptions:
+        Functional form of wage and utility follows the arguments in my Thesis.
+        Particularly: Penalty term is sum of logs of worktime shares.
 
     Thoughts:
     Currently, all wages will be very similar as prices are the same for all i
@@ -20,46 +28,59 @@ def mc_optimal_wage_choice():
     '''
     # import packages
     import numpy as np
+    import pandas as pd
     from scipy.optimize import minimize
 
     # import other modules
     from mc_skills import draw_skills
     from mc_prices import draw_skill_prices
 
-    # Set seed.
-    seed = 555
-    np.random.seed(seed)
-
-    # Set parameters
-    n = 20
-
     # Draw simulated skills and prices
-    sim_skills = draw_skills(n=n, seed=seed)
-    sim_prices = draw_skill_prices()
+    sim_skills = draw_skills(n=n, J=J, seed=seed)
+    sim_prices = draw_skill_prices(T=T, J=J, seed=seed)
 
     # Define individual wage as a function of worktime shares
-    def wage(lmb, i):
-        return lmb*(sim_skills[i, :] + sim_prices[:, i])[0] \
-            + (1-lmb)*(sim_skills[i, :] + sim_prices[:, i])[1]
+    def wage(lmb, i, t):
+        return lmb*(sim_skills[i, :] + sim_prices[:, t])[0] \
+            + (1-lmb)*(sim_skills[i, :] + sim_prices[:, t])[1]
 
     # Define utility as a function of wage and worktime shares
     # Note: defined negative to use minimizing function
-    def utility(lmb, i):
-        return -1*(wage(lmb, i) + np.log(lmb) + np.log(1-lmb))
+    def utility(lmb, i, t):
+        return -1*(wage(lmb, i, t) + np.log(lmb) + np.log(1-lmb))
 
     # Find optimal worktime shares lmb, according wages and untility
-    lmb_opt, util_opt, wage_opt = np.empty([3, n])
+    lmb_opt = np.empty([T, n])
+    util_opt = np.empty([T, n])
+    wage_opt = np.empty([T, n])
     for i in range(n):
-        opt = minimize(
-            fun=utility,
-            x0=0.5,
-            args=i,
-            method='SLSQP',
-            bounds=[(0, 1)],
-            options={"maxiter": 15}
-            )
-        lmb_opt[i] = opt["x"]
-        util_opt[i] = (-1)*opt["fun"]
-        wage_opt[i] = wage(lmb_opt[i], n)
+        for t in range(T):
+            opt = minimize(
+                fun=utility,
+                x0=0.5,
+                args=(i, t),
+                method='SLSQP',
+                bounds=[(0.01, 0.99)],
+                options={"maxiter": 15}
+                )
+            lmb_opt[t, i] = opt["x"]
+            util_opt[t, i] = (-1)*opt["fun"]
+            wage_opt[t, i] = wage(lmb_opt[t, i], i, t)
 
-    return lmb_opt, util_opt, wage_opt
+    # Initialize DataFrame with simulated data
+    # define multiindex
+    index = pd.MultiIndex(
+                     levels=[list(range(n)), ["lambda", "wage", "utility"]],
+                     codes=[list(np.repeat(range(n), 3)), [0, 1, 2]*n]
+                     )
+
+    # write simulated data into multiindexed dataframe
+    mc_data = pd.DataFrame(index=index, columns=list(range(T)))
+    idx = pd.IndexSlice
+
+    for t in range(T):
+        mc_data.loc[idx[:, "lambda"], t] = lmb_opt[t]
+        mc_data.loc[idx[:, "wage"], t] = wage_opt[t]
+        mc_data.loc[idx[:, "utility"], t] = util_opt[t]
+
+    return mc_data
