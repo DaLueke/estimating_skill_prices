480b6048c5d7b495c067a50161c3c732aa87d888
Daniel Lüke <daniel.lueke@t-online.de>
2020-02-05 17:03:35 +0100

    Write rslt for M MC-iterations into json file.

--- MC_simulation/MC_estimation.py
+++ MC_simulation/MC_estimation.py
@@ -14,11 +14,12 @@ from mc_prices import draw_skill_prices
 pd.set_option('display.max_rows', None, 'display.max_columns', None)
 
 # Define indexer and parameters
-# set seed for DGP
-seed = 600
-idx = pd.IndexSlice
+seed = 600      # set seed for Data Generating Process
+N = 100          # number of observations
+M = 10          # number of MC interations
+idx = pd.IndexSlice     # define Indexslice
 
-# Define optional arguments
+# Define optional arguments of DGP.
 kwargs = {
           # (1) Arguments for price changing process.
           "pi_fun": "pi_fixed",
@@ -37,81 +38,92 @@ penalty_power = [1.25, 1.5, 2, 2.5, 3]
 rslt = pd.DataFrame(index=penalty_power, columns=loci)
 rslt_difference = pd.DataFrame(index=penalty_power, columns=loci)
 count_corner_sol = pd.DataFrame(index=penalty_power, columns=loci)
+task_adjustments = pd.DataFrame(index=penalty_power, columns=loci)
 # corr_lmb_bar_d_lmb = pd.DataFrame(index=penalty_power, columns=loci)
 # corr_d_w_d_lmb = pd.DataFrame(index=penalty_power, columns=loci)
 # corr_d_w_lmb_bar = pd.DataFrame(index=penalty_power, columns=loci)
-task_adjustments = pd.DataFrame(index=penalty_power, columns=loci)
 
 # calculate true parameters for estimators
 baseline = (Dpi_1, (Dpi_2-Dpi_1))
 
-# Simulate data and estimate for each combination of locus and penalty term
-for l in range(0, len(loci)):
-    for p in range(0, len(penalty_power)):
-        # Simulate data
-        mc_data = mc_optimal_wage_choice(
-            n=100,
-            T=2, J=2,
-            penalty='quad',
-            p_weight=10,
-            p_locus=loci[l],
-            p_exponent=penalty_power[p],
-            seed=seed,
-            **kwargs
-            )
+# empty dict for Results
+rslt_dict = {}
 
-        # count the number of cornersolutions
-        corner_0 = sum(mc_data.loc[idx[:, "lambda"], 0] == 0) \
-            + sum(mc_data.loc[idx[:, "lambda"], 0] == 1)
+# Run MC Simulation m times.
+for m in range(0, M):
+    # Simulate data and estimate for each combination of locus and penalty term
+    for l in range(0, len(loci)):
+        for p in range(0, len(penalty_power)):
+            # Simulate data
+            mc_data = mc_optimal_wage_choice(
+                n=100,
+                T=2, J=2,
+                penalty='quad',
+                p_weight=10,
+                p_locus=loci[l],
+                p_exponent=penalty_power[p],
+                seed=seed,
+                **kwargs
+                )
 
-        corner_1 = sum(mc_data.loc[idx[:, "lambda"], 1] == 0) \
-            + sum(mc_data.loc[idx[:, "lambda"], 1] == 1)
+            # count the number of cornersolutions
+            corner_0 = sum(mc_data.loc[idx[:, "lambda"], 0] == 0) \
+                + sum(mc_data.loc[idx[:, "lambda"], 0] == 1)
 
-        count_corner_sol.iloc[p, l] = (corner_0, corner_1)
+            corner_1 = sum(mc_data.loc[idx[:, "lambda"], 1] == 0) \
+                + sum(mc_data.loc[idx[:, "lambda"], 1] == 1)
 
-        # estimate price changes
-        # 1. calculate approximated lambda
-        lmb_bar = np.array((mc_data.loc[idx[:, "lambda"], 0]
-                           + mc_data.loc[idx[:, "lambda"], 1])/2).astype(float)
+            count_corner_sol.iloc[p, l] = (corner_0, corner_1)
 
-        # 2. get true wage difference
-        wage_change = (mc_data.loc[idx[:, "wage"], 1]
-                       - mc_data.loc[idx[:, "wage"], 0])
+            # estimate price changes
+            # 1. calculate approximated lambda
+            lmb_bar = np.array((mc_data.loc[idx[:, "lambda"], 0]
+                               + mc_data.loc[idx[:, "lambda"], 1])/2).astype(float)
 
-        # run estimation
-        beta0, beta1 = sms.OLS(
-               endog=np.array(wage_change).astype(float),
-               exog=sms.add_constant(lmb_bar),
-               ).fit().params.round(4)
+            # 2. get true wage difference
+            wage_change = (mc_data.loc[idx[:, "wage"], 1]
+                           - mc_data.loc[idx[:, "wage"], 0])
 
-        # calculate error in estimated coefficients
-        rslt.iloc[p, l] = (beta0, beta1)
-        rslt_difference.iloc[p, l] = np.round((beta0-baseline[0],
-                                               beta1-baseline[1]), 4)
+            # run estimation
+            beta0, beta1 = sms.OLS(
+                   endog=np.array(wage_change).astype(float),
+                   exog=sms.add_constant(lmb_bar),
+                   ).fit().params.round(4)
 
-        # calculate correlation between lmb_bar and D_lmb
-        D_lmb = np.array(mc_data.loc[idx[:, "lambda"], 1]
-                         - mc_data.loc[idx[:, "lambda"], 0]).astype(float)
+            # calculate error in estimated coefficients
+            rslt.iloc[p, l] = (beta0, beta1)
+            rslt_difference.iloc[p, l] = np.round((beta0-baseline[0],
+                                                   beta1-baseline[1]), 4)
 
-        # As a quality check, take a look at correlation coefficients.
-        # corr_lmb_bar_d_lmb.iloc[p, l] = np.corrcoef(lmb_bar, D_lmb).round(4)
-        # corr_d_w_d_lmb.iloc[p, l] = np.corrcoef(np.array(wage_change
-        #                                                 ).astype(float),
-        #                                        D_lmb
-        #                                        ).round(4)
+            # calculate correlation between lmb_bar and D_lmb
+            D_lmb = np.array(mc_data.loc[idx[:, "lambda"], 1]
+                             - mc_data.loc[idx[:, "lambda"], 0]).astype(float)
 
-        # corr_d_w_lmb_bar.iloc[p, l] = np.corrcoef(np.array(wage_change
-        #                                                   ).astype(float),
-        #                                          lmb_bar).round(4)
+            # As a quality check, take a look at correlation coefficients.
+            # corr_lmb_bar_d_lmb.iloc[p, l] = np.corrcoef(lmb_bar, D_lmb).round(4)
+            # corr_d_w_d_lmb.iloc[p, l] = np.corrcoef(np.array(wage_change
+            #                                                 ).astype(float),
+            #                                        D_lmb
+            #                                        ).round(4)
 
-        # store task adjustments
-        task_adjustments.iloc[p, l] = np.mean(D_lmb).round(4)
+            # corr_d_w_lmb_bar.iloc[p, l] = np.corrcoef(np.array(wage_change
+            #                                                   ).astype(float),
+            #                                          lmb_bar).round(4)
+
+            # store task adjustments
+            task_adjustments.iloc[p, l] = np.mean(D_lmb).round(4)
+    d = {m: rslt_difference.to_json()}
+    rslt_dict.update(d)
 path="C:/Users/danie/Documents/Master_Bonn/5_Semester/Thesis/social_skill_prices/MC_simulation/OUT/"
 
 rslt_difference.to_json(
                         path_or_buf=path+"rslt_difference.json",
                         orient="index",
                         )
+with open(path+"rslt_dict.json", "w") as json_file:
+    json.dump(rslt_dict, json_file)
+
+
 """
 # Print results
 print("estimation coefficients: ", "\n",
