329a33872bee16f636e101651ed511ae2d4ddb96
Daniel Lüke <daniel.lueke@t-online.de>
2020-02-06 11:48:02 +0100

    Implement function for fixed price changes.

--- MC_simulation/mc_prices.py
+++ MC_simulation/mc_prices.py
@@ -1,16 +1,33 @@
-def draw_skill_prices(T, J, seed):
+
+
+def draw_skill_prices(
+    T, J, seed,
+    pi_fun='pi_fixed',
+    low=-0.2,
+    high=0.2,
+    const=[0.05, 0.1]
+):
     """ Draws initial skill prices and simulates random prices changes.
     Agruments:
-        T              Number of periods
-        J              Number of tasks
-        seed           Seed for random draw of prices
+        T              (int) Number of periods
+        J              (int) Number of tasks
+        seed           (int) Seed for random draw of prices
+        pi_fun         (str) defines the process of wage changes. Currently
+                       implemented options:
+                       - pi_normal: Draws from standard normal distribution.
+                       - pi_uniform: Draws uniform distribution. Borders
+                         are defined in "low" and "high" arguments.
+                       - pi_fixed: Non-random, constant price changes.
+                         Changes can be provided in "const" argument.
+        low            (int) Lower border of uniform distributed price changes.
+        high           (int) Upper border of uniform distributed price changes.
+        const          (list) Changes for pi_fixed option.
 
     Returns:
-        pi1, pi2       Prices for tasks 1 and 2
+        pi1, pi2       JxT array of prices for tasks 1 and 2.
 
     Assumptions:
         (1) Initial Skill Prices
-        (2) Chages in Skill Prices are normal distributed
     """
     # import packages
     import numpy as np
@@ -18,26 +35,44 @@ def draw_skill_prices(T, J, seed):
     # set seed
     np.random.seed(seed)
 
+    # define functions that return price changes for different specifications
+    # (1) Draw stadard normal distributed changes in log prices.
+    def pi_normal(J=J, T=T, **kwargs):
+        pi_normal = np.around(np.random.normal(size=(J, T-1)), 4)
+        return pi_normal
+
+    # (2) Draw changes in log prices that are uniformly distributed over
+    # some interval.
+    def pi_uniform(J=2, T=T, **kwargs):
+        low, high = kwargs['low'], kwargs['high']
+        pi_uniform = np.around(
+            np.random.uniform(low, high, size=(J, T-1)),
+            4
+            )
+        return pi_uniform
+
+    # (3) Fix changes in log prices.
+    def pi_fixed(J=J, T=T, **kwargs):
+        const = kwargs['const']
+        pi_fixed = np.array([const, ]*T).transpose()
+        return pi_fixed
+
     # Set initial task prices
     # Assume task 1 (social) has a lower price than 2 (non-social)
     pi1_0 = 5
     pi2_0 = 6
 
-    # Draw stadard normal distributed changes in log prices
-    d_pi_normal = np.around(np.random.normal(size=(J, T-1)), 4)
-
-    # Draw changes in log prices that are uniformly distributed in [-0.2, 0.2]
-    d_pi_uniform = np.around(
-        np.random.uniform(low=-0.2, high=0.2, size=(J, T-1)),
-        4
-        )
-
     # Define price array
     pi = np.empty([J, T])
     pi[:, 0] = pi1_0, pi2_0
 
+    # Get price changes.
+    # Find price changes function of choice:
+    price_changes = eval(pi_fun)
+    d_pi = price_changes(T=T, J=J, low=low, high=high, const=const)
+
     # Calculate prices in each period
     for t in range(1, T):
-        pi[:, t] = pi[:, t-1] + d_pi_uniform[:, t-1]
+        pi[:, t] = pi[:, t-1] + d_pi[:, t-1]
 
     return pi
