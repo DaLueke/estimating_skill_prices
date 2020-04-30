45743185e03648e4d22449389826efc7a2da56d0
Daniel Lüke <daniel.lueke@t-online.de>
2020-04-30 23:45:47 +0200

    Clean code and add correlation heatmap.

--- MC_simulation/mc_estimation.py
+++ MC_simulation/mc_estimation.py
@@ -17,7 +17,6 @@ import os
 
 from DGP.draw_data import draw_simulation_data
 
-
 # Make this function callable
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
@@ -33,13 +32,16 @@ sys.path.append(os.path.abspath(os.getcwd()) + "\\DGP")
 kwargs = {
           # (1) Arguments for price changing process.
           "pi_fun": "pi_fixed",
-          "const": [0.0, 0.05]
+          "const": [0.0, 0.1]
           }
 # set number of MC iterations
-M = 500
+np.random.seed(100)
+M = 1000
+N = 100
 
 # initialize result arrays
 utility_on_mean_lmb = np.empty(M)
+adj_wage_on_mean_lmb = np.empty(M)
 wage_on_mean_lmb = np.empty(M)
 wage_on_mean_and_diff_lmb = np.empty(M)
 
@@ -47,159 +49,229 @@ wage_on_mean_and_diff_lmb = np.empty(M)
 for m in range(M):
     # Draw simulatioon data
     sim_data = draw_simulation_data(
-        T=2,
-        N=500,
+        T=3,
+        N=N,
         J=2,
         penalty="quad",
-        p_weight=35,
+        p_weight=15,
         p_locus=(0.3, 0.7),
         p_exponent=2,
         **kwargs
     )
 
     idx = pd.IndexSlice
-    mean_lmb = np.array(
+
+    # Calculate mean task choice in base period
+    mean_lmb_base = np.array(
         sim_data.loc[idx[0, :], "lambda"].values +
         sim_data.loc[idx[1, :], "lambda"].values
         )/2
 
-    diff_lmb = np.array(
+    # Calculate chage in task choice in base period
+    diff_lmb_base = np.array(
         sim_data.loc[idx[1, :], "lambda"].values -
         sim_data.loc[idx[0, :], "lambda"].values
         )
 
-    util_change = np.array(
+    # utility change in base period
+    util_change_base = np.array(
         sim_data.loc[idx[1, :], "utility"].values -
         sim_data.loc[idx[0, :], "utility"].values
         )
 
-    wage_change = np.array(
+    # wage change in base period
+    wage_change_base = np.array(
         sim_data.loc[idx[1, :], "wage"].values -
         sim_data.loc[idx[0, :], "wage"].values
         )
 
-    # Estimate specifications
-    # u ~ mean_lmb
-    tilde_pi = sms.OLS(
-        endog=util_change.astype(float),
-        exog=mean_lmb.astype(float)
-    ).fit().params.round(6)
-    utility_on_mean_lmb[m] = tilde_pi
-
-    # Debugging:
-    # print("u ~ mean_lmb \n tilde_pi ={0}".format(tilde_pi))
-
-    # w ~ mean_lmb
-    tilde_pi = sms.OLS(
-        endog=wage_change.astype(float),
-        exog=mean_lmb.astype(float)
-    ).fit().params.round(6)
-    wage_on_mean_lmb[m] = tilde_pi
-
-    # Debugging:
-    # print("w ~ mean_lmb \n tilde_pi ={0}".format(tilde_pi))
-
-    # #### w ~ c + mean_lmb
-    # pi_1, tilde_pi = sms.OLS(
-    #     endog=wage_change.astype(float),
-    #     exog=sms.add_constant(mean_lmb.astype(float))
-    # ).fit().params.round(4)
-    # print("In a model that only uses mean task choice as predictor of wage",
-    #       " changes, estimated rel. wage change is {1}".format(pi_1, tilde_pi))
-
-    #### w ~ mean_lmb + diff_lmb
-    exog = pd.DataFrame(data={
-        "mean_lmb": mean_lmb.astype(float),
-        "diff_lmb": diff_lmb.astype(float)
+    # Estimate changes in amenities and skill accumulation in base period
+    exog_base = pd.DataFrame(data={
+        "const": np.ones(N),
+        "mean_lmb_base": mean_lmb_base.astype(float),
+        "diff_lmb_base": diff_lmb_base.astype(float),
+        "lmb_inter_base": mean_lmb_base.astype(float) *
+        diff_lmb_base.astype(float),
+        "lmb_0": sim_data.loc[idx[0, :], "lambda"].values.astype(float),
+        "lmb_0^2": sim_data.loc[idx[0, :], "lambda"].values.astype(float)**2,
+        "lmb_1": sim_data.loc[idx[1, :], "lambda"].values.astype(float),
+        "lmb_1^2": sim_data.loc[idx[1, :], "lambda"].values.astype(float)**2,
+        "mean_lmb_inter_lmb_0": mean_lmb_base.astype(float) *
+        sim_data.loc[idx[0, :], "lambda"].values.astype(float),
         })
-    tilde_pi, v = sms.OLS(
-        endog=wage_change.astype(float),
-        exog=exog[["mean_lmb", "diff_lmb"]]
-    ).fit().params.round(6)
-    wage_on_mean_and_diff_lmb[m] = tilde_pi
-    # print("In a model that uses differences in lmb in addition to mean task ",
-    #       "choice as predictor of wage changes, estimated rel. wage change is",
-    #       " {1}. The amentity coefficient is {2}.".format(pi_1, tilde_pi, v)
-    #       )
 
-    # #### Correcting wages for changes in amenities
-    # corrected_wage = wage_change.astype(float) - v * diff_lmb.astype(float)
-    # pi_1, tilde_pi = sms.OLS(
-    #     endog=corrected_wage.astype(float),
-    #     exog=sms.add_constant(mean_lmb.astype(float))
-    # ).fit().params.round(4)
-    # print("Correcting wage change by the estimated amentiy change and using only",
-    #       " mean task choice as predictor of wage",
-    #       " changes, estimated rel. wage change is {1}".format(pi_1, tilde_pi))
+    OLS_base_period_result = sms.OLS(
+        endog=wage_change_base.astype(float),
+        exog=exog_base[[
+            # "const",
+            # "lmb_0",
+            "lmb_1",
+            # "lmb_0^2",
+            "lmb_1^2",
+            # "mean_lmb_base",
+            # "diff_lmb_base",
+            # "lmb_inter_base"
+            ]]
+        ).fit()
 
+    # OLS_base_period_result.summary()
+    # print(OLS_base_period_result.summary().as_latex())
 
-    # #### including an interacting term of mean_lmb and diff_lmb
-    # #### w ~ c + mean_lmb + diff_lmb + mean_lmb*diff_lmb
-    # exog["inter"] = (mean_lmb * diff_lmb).astype(float)
-    # pi_1, tilde_pi, inter = sms.OLS(
-    #     endog=wage_change.astype(float),
-    #     exog=exog[["C", "mean_lmb", "inter"]]
-    # ).fit().params.round(4)
-    # print("w ~ c + mean_lmb + diff_lmb + mean_lmb*diff_lmb \n",
-    #       "tilde_pi = {0} \n inter = {1}".format(tilde_pi, inter), "\n")
+    # Use estimation result to adjust wages changes for skill and
+    # amenity changes in subsequent periods
 
-# Plot estimator for mean_lmb from w ~ mean_lmb
+    # Calculate mean task choice in estimation period
+    mean_lmb_est = np.array(
+        sim_data.loc[idx[2, :], "lambda"].values +
+        sim_data.loc[idx[1, :], "lambda"].values
+        )/2
+
+    # Calculate chage in task choice in estimation period
+    diff_lmb_est = np.array(
+        sim_data.loc[idx[2, :], "lambda"].values -
+        sim_data.loc[idx[1, :], "lambda"].values
+        )
+
+    # wage change in estimation period
+    wage_change_est = np.array(
+        sim_data.loc[idx[2, :], "wage"].values -
+        sim_data.loc[idx[1, :], "wage"].values
+        )
+
+    # wage change in estimation period
+    util_change_est = np.array(
+        sim_data.loc[idx[2, :], "utility"].values -
+        sim_data.loc[idx[1, :], "utility"].values
+        )
+
+    # Write exogeneous variables into df
+    exog_est = pd.DataFrame(data={
+        "const": np.ones(N),
+        "mean_lmb_est": mean_lmb_est.astype(float),
+        "diff_lmb_est": diff_lmb_est.astype(float),
+        "lmb_inter_est": mean_lmb_est.astype(float)*diff_lmb_est.astype(float),
+        "lmb_1": sim_data.loc[idx[1, :], "lambda"].values.astype(float),
+        "lmb_1^2": sim_data.loc[idx[1, :], "lambda"].values.astype(float)**2,
+        "lmb_2": sim_data.loc[idx[2, :], "lambda"].values.astype(float),
+        "lmb_2^2": sim_data.loc[idx[2, :], "lambda"].values.astype(float)**2,
+        "mean_lmb_inter_lmb_1": mean_lmb_est.astype(float) *
+        sim_data.loc[idx[1, :], "lambda"].values.astype(float),
+        })
+
+    # Adjust wage change for predicted changes in skills and amentities
+    wage_change_est_adjusted = wage_change_est - \
+        OLS_base_period_result.predict(
+            exog_est[[
+                # "mean_lmb_est",
+                # "const",
+                # "lmb_1",
+                "lmb_2",
+                # "lmb_1^2",
+                "lmb_2^2",
+                # "diff_lmb_est",
+                # "lmb_inter_est"
+                ]]
+            )
+
+    # Regress adjusted wage changes on mean_lmb
+    pi_tilde = sms.OLS(
+        endog=wage_change_est_adjusted.astype(float),
+        exog=mean_lmb_est.astype(float)
+        ).fit().params.round(6)
+    adj_wage_on_mean_lmb[m] = pi_tilde
+
+    # Regress non-adjusted wages on mean_lmb
+    # pi_tilde = sms.OLS(
+    #     endog=wage_change_est.astype(float),
+    #     exog=mean_lmb_est.astype(float)
+    #     ).fit().params.round(6)
+    # wage_on_mean_lmb[m] = pi_tilde
+
+###############
+#### Plots ####
+###############
+
+# Plot estimator for mean_lmb from adjusted w ~ mean_lmb
 plt.rc('axes', axisbelow=True)
 plt.xticks(rotation=45)
 plt.xlabel("estimated rel. price change")
 plt.ylabel("frequency")
 plt.rc('grid', linestyle="-", color='black')
 plt.grid(True)
-plt.hist(wage_on_mean_lmb, bins=30, range=(0.0495, 0.051))
+plt.hist(adj_wage_on_mean_lmb, bins=1000, range=(-0.05, 0.05))
 plt.figtext(
     x=0,
     y=-0.25,
     s='Distribution of the estimated change in relative skill prices with ' +
       'realized wage as regressand \n' +
       'and mean lambda as regressor' +
-      'in a Monte Carlo Simulation with M = 500 repititions \nand N = 500 ' +
+      'in a Monte Carlo Simulation with M = 500 repititions \nand N = 300 ' +
       'simulated individuals.' +
       'True relative price change in the underlying DGP: 0.05.'
 )
 plt.savefig("FIG/MC_rslt_wage_on_mean_lmb.png", bbox_inches="tight")
 
-# Plot estimator for mean_lmb from w ~ mean_lmb + diff_lmb
+# Plot estimator for mean_lmb from non-adjusted w ~ mean_lmb
 plt.rc('axes', axisbelow=True)
 plt.xticks(rotation=45)
 plt.xlabel("estimated rel. price change")
 plt.ylabel("frequency")
 plt.rc('grid', linestyle="-", color='black')
 plt.grid(True)
-plt.hist(wage_on_mean_and_diff_lmb, bins=30, range=(0.0495, 0.0515))
+plt.hist(wage_on_mean_lmb, bins=100, range=(0.175, 0.225))
 plt.figtext(
     x=0,
     y=-0.25,
     s='Distribution of the estimated change in relative skill prices with ' +
       'realized wage as regressand \n' +
-      'and mean lambda as well as changes in lmb as regressors ' +
-      'in a Monte Carlo Simulation with M = 500 repititions \nand N = 500 ' +
-      'simulated individuals. ' +
+      'and mean lambda as regressor' +
+      'in a Monte Carlo Simulation with M = 500 repititions \nand N = 300 ' +
+      'simulated individuals.' +
       'True relative price change in the underlying DGP: 0.05.'
 )
-plt.savefig("FIG/MC_rslt_wage_on_mean_and_diff_lmb.png", bbox_inches="tight")
 
 
-# Plot estimator for mean_lmb from u ~ mean_lmb
-plt.rc('axes', axisbelow=True)
-plt.xticks(rotation=45)
-plt.xlabel("estimated rel. price change")
-plt.ylabel("frequency")
-plt.rc('grid', linestyle="-", color='black')
-plt.grid(True)
-plt.hist(utility_on_mean_lmb, bins=30, range=(0.0495, 0.051))
-plt.figtext(
-    x=0,
-    y=-0.25,
-    s='Distribution of the estimated change in relative skill prices with ' +
-      'utility as regressand \n' +
-      'and mean lambda as regressor ' +
-      'in a Monte Carlo Simulation with M = 500 repititions \nand N = 500 ' +
-      'simulated individuals. ' +
-      'True relative price change in the underlying DGP: 0.05.'
-)
-plt.savefig("FIG/MC_rslt_utility_on_mean_lmb.png", bbox_inches="tight")
+
+
+
+#
+# # Plot estimator for mean_lmb from w ~ mean_lmb + diff_lmb
+# plt.rc('axes', axisbelow=True)
+# plt.xticks(rotation=45)
+# plt.xlabel("estimated rel. price change")
+# plt.ylabel("frequency")
+# plt.rc('grid', linestyle="-", color='black')
+# plt.grid(True)
+# plt.hist(wage_on_mean_and_diff_lmb, bins=30, range=(0.0495, 0.0515))
+# plt.figtext(
+#     x=0,
+#     y=-0.25,
+#     s='Distribution of the estimated change in relative skill prices with ' +
+#       'realized wage as regressand \n' +
+#       'and mean lambda as well as changes in lmb as regressors ' +
+#       'in a Monte Carlo Simulation with M = 500 repititions \nand N = 500 ' +
+#       'simulated individuals. ' +
+#       'True relative price change in the underlying DGP: 0.05.'
+# )
+# plt.savefig("FIG/MC_rslt_wage_on_mean_and_diff_lmb.png", bbox_inches="tight")
+#
+#
+# # Plot estimator for mean_lmb from u ~ mean_lmb
+# plt.rc('axes', axisbelow=True)
+# plt.xticks(rotation=45)
+# plt.xlabel("estimated rel. price change")
+# plt.ylabel("frequency")
+# plt.rc('grid', linestyle="-", color='black')
+# plt.grid(True)
+# plt.hist(utility_on_mean_lmb, bins=30, range=(0.0495, 0.051))
+# plt.figtext(
+#     x=0,
+#     y=-0.25,
+#     s='Distribution of the estimated change in relative skill prices with ' +
+#       'utility as regressand \n' +
+#       'and mean lambda as regressor ' +
+#       'in a Monte Carlo Simulation with M = 500 repititions \nand N = 500 ' +
+#       'simulated individuals. ' +
+#       'True relative price change in the underlying DGP: 0.05.'
+# )
+# plt.savefig("FIG/MC_rslt_utility_on_mean_lmb.png", bbox_inches="tight")
